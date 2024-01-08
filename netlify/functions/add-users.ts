cbd4079a28cb263c075b5a892bfe42a60f4c02bf
Daniel C <daniel@clayson.io>
2024-01-08 09:46:26 -0500

    Add Users

---
+++ netlify/functions/add-users.ts
@@ -0,0 +1,32 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import {MongoDB} from "./Utilities/MongoDB";
+import {z} from "zod";
+
+const userSchema = z.object({
+    email: z.string().email(),
+    _id: z.string().optional(),
+})
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    const { user } = context.clientContext!;
+    if(!user) return {statusCode: 401};
+    const mongo = new MongoDB({});
+    const {document: dbUser} = await mongo.FindOne<{document: { _id: string, email: string, isAdmin: boolean }}>({collection: 'quick-maps_users', filter: {email: user.email}});
+
+    if(!dbUser?.isAdmin) return {statusCode: 401};
+
+
+    const documents = JSON.parse(event.body ?? '{}').users ?? [];
+
+    const users = z.array(userSchema).parse(documents.map(x=>({email: x.toLowerCase()})));
+
+    // Add addresses to the database
+    await mongo.InsertMany({documents: users, collection: 'quick-maps_users'});
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({}),
+    };
+};
+
+export { handler };
--- src/Pages/SettingsPage.tsx
+++ src/Pages/SettingsPage.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {IAddress, useAddresses} from "../Hooks/useAddresses.ts";
 import XLSX from "xlsx";
-import {Button} from "@mui/material";
+import {Button, TextField} from "@mui/material";
+import axios from "axios";
+import netlifyIdentity from "netlify-identity-widget";
 
 export function SettingsPage() {
     const fileRef = React.useRef<HTMLInputElement>(null);
     const nameFileRef = React.useRef<HTMLInputElement>(null);
 
     const {addresses, setAddresses} = useAddresses();
-    const uploadAddresses = async (e: any)=>{
+    const uploadAddresses = async (e: any) => {
         const f = e.target.files?.[0];
 
         const data = await f.arrayBuffer();
@@ -17,7 +19,7 @@ export function SettingsPage() {
 
         const json = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]])
 
-        const newAddresses = json.map((x: any, index: number)=>({
+        const newAddresses = json.map((x: any, index: number) => ({
             _id: index.toString(),
             address: x.Location,
             names: [],
@@ -30,24 +32,27 @@ export function SettingsPage() {
 
         setAddresses(newAddresses)
     };
-    const addNames = async (e: any)=>{
+    const addNames = async (e: any) => {
         const f = e.target.files?.[0];
 
         const data = await f.arrayBuffer();
         /* data is an ArrayBuffer */
         const workbook = XLSX.read(data);
 
-        const json = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]) as {Name: string, Address: string}[]
+        const json = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]) as {
+            Name: string,
+            Address: string
+        }[]
 
         console.log("JSON", json);
-        const newAddresses = addresses.slice().map(x=>({...x, names: []})) as IAddress[];
+        const newAddresses = addresses.slice().map(x => ({...x, names: []})) as IAddress[];
 
-        json.forEach(({Name, Address})=>{
-            if(!Name || !Address) return;
-            const address = newAddresses.find(x=>{
+        json.forEach(({Name, Address}) => {
+            if (!Name || !Address) return;
+            const address = newAddresses.find(x => {
                 return x.address?.trim().toLowerCase().startsWith(Address.toLowerCase().trim())
             });
-            if(address){
+            if (address) {
                 address.names.push(Name);
             }
         });
@@ -55,18 +60,35 @@ export function SettingsPage() {
         setAddresses(newAddresses);
     };
 
-    return <div style={{padding: '1em'}}>
+    const [users, setUsers] = useState<string>('')
 
+    function addUsers(){
+        const splitUsers = users.split('\n')
 
+        fetch('/.netlify/functions/add-users', {
+            method: 'POST',
+            body: JSON.stringify({users: splitUsers}),
+            headers: {
+                Authorization: `Bearer ${netlifyIdentity.currentUser()?.token?.access_token}`
+            }
+        });
+    }
 
-            <Button variant={'contained'} onClick={()=>fileRef.current?.click()}>
-                Add Addresses
-            </Button>
-            <input ref={fileRef} type={'file'} onChange={uploadAddresses} style={{display: 'none'}}/>
+    return <div style={{padding: '1em', display: 'flex', flexDirection: 'column', gap: '1em'}}>
+        <h3>Note: This page will only work if you're the creator of this site</h3>
 
-            <Button variant={'contained'} onClick={()=>nameFileRef.current?.click()}>
-                Upload Names
-            </Button>
-            <input ref={nameFileRef} type={'file'} onChange={addNames} style={{display: 'none'}}/>
-  </div>;
+        <Button variant={'contained'} onClick={() => fileRef.current?.click()}>
+            Add Addresses
+        </Button>
+        <input ref={fileRef} type={'file'} onChange={uploadAddresses} style={{display: 'none'}}/>
+
+        <Button variant={'contained'} onClick={() => nameFileRef.current?.click()}>
+            Upload Names
+        </Button>
+        <input ref={nameFileRef} type={'file'} onChange={addNames} style={{display: 'none'}}/>
+
+        <TextField label={'Users'} multiline={true} rows={5} value={users} onChange={(e)=>setUsers(e.target.value)}/>
+        <Button variant={'contained'} onClick={addUsers}>Add Users</Button>
+
+    </div>;
 }
